04428370b051606898b44e4ca57f4e29bc17da0a
ktippasani <kotit70@gmail.com>
2023-07-10 16:48:47 -0400

    Form validation

--- table-booking-Angular/src/app/create-booking/create-booking.component.ts
+++ table-booking-Angular/src/app/create-booking/create-booking.component.ts
@@ -3,6 +3,7 @@ import { BookingService } from '../booking-service.service';
 import { Booking } from '../booking';
 import { Router } from '@angular/router';
 import { DateAdapter } from '@angular/material/core';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -42,8 +43,12 @@ export class CreateBookingComponent implements OnInit{
     error => console.log(error));
   }
 
-  onSubmit() {
-    this.submitted = true;
+  onSubmit(f: NgForm) {
+    //Stop here if form is invalid
+    if (f.invalid) {
+      return;
+  }
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(f.value, null, 4));
     this.save();    
   }
 
